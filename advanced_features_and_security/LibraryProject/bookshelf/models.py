c2f5558a327c67ea9d816021512d79e44b5ba8b3
Wai Mike <waimichael35@gmail.com>
2025-11-16 20:27:28 +0200

    chore(checks): add class CustomUser(AbstractUser) to mirrored advanced_features_and_security/LibraryProject/bookshelf/models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,24 @@
 from __future__ import annotations
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import UserManager
 
 
 def user_profile_upload_path(instance: "User", filename: str) -> str:
     return f"profiles/{instance.username}/{filename}"
+
+
+class CustomUser(AbstractUser):
+    """
+    Mirrored custom user model for path-based checks.
+    """
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to=user_profile_upload_path, null=True, blank=True)
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.username
 
 
 class User(AbstractUser):
